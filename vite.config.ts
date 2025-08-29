caec4076a7351c43fffe6f26677ad79ff38fd143
ToanLe <lequytoanptit0303@gmail.com>
2025-08-30 00:07:46 +0700

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import version from "vite-plugin-package-version";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), version()],
+  base: "/CmsnChiTrang/",
   build: {
     rollupOptions: {
       output: {
